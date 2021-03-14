daaca806cc032db91539adb67cc9bdd7557103e3
� Simone <simonetedesco.dev@gmail.com>
2021-03-14 18:44:01 +0100

    added rotation to planet

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import gsap from "gsap";
 import { Controls, PlayState, Tween } from "react-gsap";
+import planetImg from "./static/mission_pianeta_p24.png";
 
 function App() {
   // useEffect(() => gsap.to("#logo", { duration: 2, y: 330 }));
+  const [planetRotation, setPlanetRotation] = useState(0);
   const moveLogo = () => {
     gsap.to(".App-logo", { duration: 2, y: 800 });
+  };
+
+  const rotate = (deg) => {
+    gsap.to(".planet-img", { rotate: deg, duration: 1 });
+  };
+  const goBack = () => {
+    setPlanetRotation(planetRotation - 90);
+  };
+  const goForward = () => {
+    setPlanetRotation(planetRotation + 90);
   };
   return (
     <div className="App">
@@ -54,8 +66,35 @@ function App() {
           />
         </Tween> */}
       </header>
-      <section>
-        <div className="planet" />
+      <div className="buttons">
+        <button onClick={goBack}>indietro</button>
+        <button onClick={goForward}>avanti</button>
+      </div>
+      <section
+        style={{
+          overflow: "hidden",
+          display: "flex",
+          justifyContent: "center",
+          maxHeight: "40vh",
+        }}
+      >
+        <Tween
+          to={{
+            width: "100vw",
+            rotation: planetRotation,
+            width: "300%",
+            height: "300%",
+          }}
+          duration={1}
+        >
+          {/* <div className="planet" /> */}
+          <img
+            src={planetImg}
+            className="planet-img"
+            alt="asd"
+            // style={{ width: "300%", height: "300%" }}
+          />
+        </Tween>
       </section>
     </div>
   );
