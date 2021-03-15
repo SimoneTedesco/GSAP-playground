0b1b4f26308dc5628fe1165a01da555a9a2f77e2
� Simone <simonetedesco.dev@gmail.com>
2021-03-15 23:37:52 +0100

    fixed rotation and button styles

--- src/App.jsx
+++ src/App.jsx
@@ -16,55 +16,18 @@ function App() {
     gsap.to(".planet-img", { rotate: deg, duration: 1 });
   };
   const goBack = () => {
-    setPlanetRotation(planetRotation - 90);
+    setPlanetRotation(planetRotation + 90);
   };
   const goForward = () => {
-    setPlanetRotation(planetRotation + 90);
+    setPlanetRotation(planetRotation - 90);
   };
   return (
     <div className="App">
       <header className="App-header">
         {/* <PlayState > */}
-        <Tween from={{ x: "100vw", y: "75vh" }} duration="2">
+        <Tween from={{ x: "100vw", y: "75vh" }} duration="2" ease="back">
           <h1 className="App-logo">SIMONE</h1>
         </Tween>
-        {/* </PlayState> */}
-        {/* <h1 className="App-logo">SIMONE</h1> */}
-        {/* <img src={logo} id="logo" className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Tween
-          to={{ x: "200px", rotation: 180 }}
-          duration={2}
-          ease="back.out(1.7)"
-        >
-          <a
-            className="App-link"
-            target="_blank"
-            rel="noopener noreferrer"
-            onClick={() => moveLogo()}
-          >
-            Learn React
-          </a>
-        </Tween>
-        <a
-          className="App-link"
-          target="_blank"
-          rel="noopener noreferrer"
-          onClick={() => moveLogo()}
-        >
-          Learn React
-        </a>
-        <Tween
-          to={{ x: "200px", rotation: 180 }}
-          duration={2}
-          ease="back.out(1.7)"
-        >
-          <div
-            style={{ width: "100px", height: "100px", background: "#ccc" }}
-          />
-        </Tween> */}
       </header>
       <div className="buttons">
         <button onClick={goBack}>indietro</button>
