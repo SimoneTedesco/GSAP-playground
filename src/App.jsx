60cec2c5d0b8125fe2c6fd972f03d8c1037ced77
� Simone <simonetedesco.dev@gmail.com>
2021-03-24 23:54:01 +0100

    test scrub animation

--- src/App.jsx
+++ src/App.jsx
@@ -132,14 +132,43 @@ function App() {
             className="boxbox"
             style={{
               height: "300px",
-              width: "300px",
+              width: "90%",
               backgroundColor: "red",
               opacity: 0,
             }}
           >
-            asd
+            asd slowmo
           </div>
         </Tween>
+      </section>
+
+      <section className="panel orange" style={{ backgroundColor: "orange" }}>
+        <p>
+          <Tween
+            from={{
+              scrollTrigger: {
+                trigger: ".orange",
+                scrub: true,
+                pin: true,
+                start: "top top",
+                end: "+=1000%",
+              },
+              scaleX: 0,
+              transformOrigin: "left center",
+              ease: "none",
+            }}
+          >
+            <span class="line line-2"></span>
+          </Tween>
+          This orange panel gets pinned when its top edge hits the top of the
+          viewport, then the line's animation is linked with the scroll position
+          until it has traveled 100% of the viewport's height (
+          <code>end: "+=100%"</code>), then the orange panel is unpinned and
+          normal scrolling resumes. Padding is added automatically to push the
+          rest of the content down so that it catches up with the scroll when it
+          unpins. You can set <code>pinSpacing: false</code> to prevent that if
+          you prefer.
+        </p>
       </section>
       {/* </Tween> */}
     </>
